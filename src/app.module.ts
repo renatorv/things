a201acebb77bc58fe0f59c168d036afe6b3bf768
renatorv <renatorv@gmail.com>
2023-07-24 14:49:44 -0300

    finalizada aula 5

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { ThingsModule } from './things/things.module';
 import { CommonModule } from './common/common.module';
 import { CategoriesModule } from './categories/categories.module';
+import { FirebaseService } from './auth/firebase/firebase.service';
+import { AutyModule } from './auty/auty.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ThingsModule, CategoriesModule, CommonModule],
+  imports: [ThingsModule, CategoriesModule, CommonModule, AutyModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, FirebaseService],
 })
 export class AppModule {}
---
+++ src/auth/firebase/firebase.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class FirebaseService {
+    async verifyToken(token: string): Promise<boolean>{
+        return true;
+    }
+}
